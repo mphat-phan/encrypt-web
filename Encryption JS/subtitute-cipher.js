464330132256de0d7f9b9cfe5284e88960822de6
mphat-phan <phanminhphat2001@gmail.com>
2022-04-13 20:06:15 +0700

    fix subtitute

--- Encryption JS/subtitute-cipher.js
+++ Encryption JS/subtitute-cipher.js
@@ -1,20 +1,27 @@
-function Cipher( input,  oldAlphabet,  newAlphabet)
+const isUpper = (str) => {
+    return !/[a-z]/.test(str) && /[A-Z]/.test(str);
+}
+const findIndexAlphabet = (str,alphabet) => {
+	for(let i=0; i<alphabet.length; i++){
+		if(str == alphabet[i]){
+			return i;
+		}
+	}
+}
+function Cipher(input,  oldAlphabet,  newAlphabet)
 {
 	var output = "";
-	var inputLen = input.size();
-	//Kiểm tra độ dài giữa plainAlphabet và cipherAlphabet
-	if (oldAlphabet.size != newAlphabet.size)
-		return "Sai";
+	var inputLen = input.length;
 
 	//Lặp n ký tự inputLen
 	for (var i = 0; i < inputLen; ++i)
 	{
-		var oldCharIndex = oldAlphabet.find(tolower(input[i]));
+		var oldCharIndex = findIndexAlphabet(input[i].toLowerCase(), oldAlphabet);
 		//Nếu tìm thấy ký tự trong oldAlphabet
 		if (oldCharIndex >= 0)
 			//Nếu tại vị trí thứ i là in hoa thì trả về bản chữ cái thay thế thứ i in hóa,
 			//ngược lại thì trả về in thường
-			output += isupper(input[i]) ? toupper(newAlphabet[oldCharIndex]) : newAlphabet[oldCharIndex];
+			output += isUpper(input[i]) ? newAlphabet[oldCharIndex].toUpperCase() : newAlphabet[oldCharIndex];
 		//Nếu không tìm thấy ký tự như là khoảng trắng thì trả về khoảng trắng hoặc các ký tự ngoài alphabet
 		else
 			output += input[i];
@@ -23,26 +30,28 @@ function Cipher( input,  oldAlphabet,  newAlphabet)
 	return output;
 }
 //Hàm mã hóa plain text
-function Encipher( input,  cipherAlphabet,  plainAlphabet)
+function Encipher(input,  _cipherAlphabet,  _plainAlphabet)
 {
 	//Từ mã plainAlphabet thay thế thành cipherAlphabet => Encipher
 	return Cipher(input, plainAlphabet, cipherAlphabet);
 }
 //Hàm giải mã cipher text
-function Decipher( input,  cipherAlphabet,  plainAlphabet)
+function Decipher(input,  _cipherAlphabet,  _plainAlphabet)
 {
 	//Từ mã cipherAlphabet thay thế thành plainAlphabet => Decipher
 	return Cipher(input, cipherAlphabet, plainAlphabet);
 }
 
 
-	//Bảng chữ cái Alphabet
-    var plainAlphabet = "abcdefghijklmnopqrstuvwxyz";
-	//Bảng chữ cái thay thế Alphabet
-    var cipherAlphabet= "xnyahpogzqwbtsflrcvmuekjdi";
-	//Plain text
-    var plainText = "ghenvoicovid";
-    
-    var cipherText = Encipher(plainText, cipherAlphabet, plainAlphabet);
-    cout << cipherText << endl;
-	cout << Decipher(cipherText, cipherAlphabet, plainAlphabet) << endl;
+//Bảng chữ cái Alphabet
+var plainAlphabet = "abcdefghijklmnopqrstuvwxyz";
+//Bảng chữ cái thay thế Alphabet
+var cipherAlphabet= "xnyahpogzqwbtsflrcvmuekjdi";
+//Plain text
+var text = "Phan Minh Phat";
+
+var cipherText = Encipher(text, cipherAlphabet, plainAlphabet);
+console.log(cipherText); 
+
+var plainText = Decipher(cipherText, plainAlphabet, cipherAlphabet);
+console.log(plainText);
